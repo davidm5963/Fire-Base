da18b5da8c666dba83d13a67968a1b0000d159dc
David Moeller <Davidmoeller115963@Gmail.com>
2018-06-29 19:35:15 -0400

    get messages from chatService onInit and onChanges

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ChatService } from '../Services/chat.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { ChatMessage } from '../Models/chat-message.model';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  feed: any[];
+
+  constructor(private chatService: ChatService) { }
 
   ngOnInit() {
+    this.chatService.getMessages().snapshotChanges()
+    .subscribe(item => {
+      this.feed = [];
+      item.forEach(element => {
+        var x = element.payload.toJSON();
+        x['$key'] = element.key;
+        this.feed.push(x);
+        })
+      });
+  }
+
+  ngOnChanges(){
+    this.ngOnInit();  
   }
 
 }
