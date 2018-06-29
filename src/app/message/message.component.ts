241a09366022b300214f374a2c7e96de72f17473
David Moeller <Davidmoeller115963@Gmail.com>
2018-06-29 19:36:39 -0400

    create fields for data to be displayed

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ChatService } from '../Services/chat.service';
+import { AuthService } from '../Services/auth.service';
+import { ChatMessage } from '../Models/chat-message.model'
 
 @Component({
   selector: 'app-message',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
+  @Input() chatMessage: ChatMessage;
+  email: string;
+  userName: string;
+  message: string;
+  timeSent: string;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(chatMessage = this.chatMessage) {
+    this.message = chatMessage.message;
+    this.email = chatMessage.email;
+    this.userName = chatMessage.userName;
+    this.timeSent = chatMessage.timeSent;
+    
   }
 
 }
