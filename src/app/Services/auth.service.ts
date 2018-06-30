4c1e131d2594da4e73627bc7b7944e7aca66df59
David Moeller <Davidmoeller115963@Gmail.com>
2018-06-30 10:44:05 -0400

    Update code to support FireStore

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,9 +1,73 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { User } from '../Models/user.model'
+
+
+@Injectable()
 export class AuthService {
 
-  constructor() { }
+  user: Observable<User>;
+
+  constructor(private afAuth: AngularFireAuth,
+              private afs: AngularFirestore,
+              private router: Router) {
+
+      // Define the user observable
+      this.user = this.afAuth.authState.pipe(
+        switchMap(user => {
+          if (user) {
+            // logged in, get custom user from Firestore
+            return this.afs.doc<User>(`users/${user.uid}`).valueChanges()
+          } else {
+            // logged out, null
+            return null
+          }
+        })
+      )
+
+  }
+
+  //// Email/Password Auth ////
+  
+  signUp(email: string, password: string) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then(user => {
+        return this.setUserDoc(user) // create initial user document
+      })
+      .catch(error => console.log(error) );
+  }
+
+  login(email: string, password: string) {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        this.router.navigate(['chat']);
+      });
+  }
+
+  // Update properties on the user document
+  updateUser(user: User, data: any) { 
+    return this.afs.doc(`users/${user.uid}`).update(data)
+  }
+
+  // Sets user data to firestore after succesful login
+  private setUserDoc(user) {
+
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+
+    const data: User = {
+      uid: user.uid,
+      email: user.email || null,
+      status: 'Online'
+    }
+
+    return userRef.set(data)
+
+  }
 }
--- src/app/Services/chat.service.ts
+++ src/app/Services/chat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth'; 
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import * as firebase from 'firebase/app';
@@ -13,28 +14,28 @@ import { ChatMessage } from '../Models/chat-message.model'
 export class ChatService {
 
   user: any;
-  chatMessages: AngularFireList<any>;
+  chatMessagesCollection: AngularFirestoreCollection<ChatMessage>;
+  chatMessages: Observable<ChatMessage[]>;
+  userName: Observable<string>;
 
-  constructor(private db: AngularFireDatabase, private afAuth: AngularFireAuth) 
+  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) 
   {
-    this.afAuth.authState.subscribe(auth =>{
-        if(auth != undefined && auth != null){
-          this.user = auth;
-        }
-      }
-    )
+    
+  }
+
+  getUser(){
+    const userId = this.user.userId;
+    const path = `users/${userId}`;
+    return this.afs.doc(path).valueChanges();
   }
 
   sendMessage(msg: string){
     const timeStamp = this.getTimeStamp();
-    //const email = this.user.email;
-    const email = "test@examplle.com";    
-    this.chatMessages = this.getMessages();
-    this.chatMessages.push({
+    const email = this.user.email;
+    this.getMessages().add({
       timeSent: timeStamp,
       message: msg,
-      //userName: this.user.userName,
-      userName: "David",      
+      userName: this.user.userName,
       email: email,
     });
 
@@ -42,7 +43,8 @@ export class ChatService {
   }
 
   getMessages(){
-    return this.db.list('messages');
+    this.chatMessagesCollection = this.afs.collection('chatMessage');
+    return this.chatMessagesCollection;
   }
 
   getTimeStamp() {
