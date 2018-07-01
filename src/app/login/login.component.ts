a92bfe83c63d2d3e6c28d08f3b5243eeea3d00b2
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-01 08:34:44 -0400

    inject services, add login(), add onSignupLinkClick()

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService} from '../Services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
 
-  constructor() { }
+  email: string;
+  password: string;
 
-  ngOnInit() {
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login()
+  {
+    this.authService.login(this.email, this.password);
+  }
+
+  onSignupLinkClick(){
+    this.router.navigate(['signup']);
   }
 
 }
