45a910da61f56eb8239d6572ac149fd02148828e
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-02 23:37:18 -0400

    Get all users on init and onchanges

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { AuthService } from '../Services/auth.service';
+import { User } from '../Models/user.model';
+
+import { Observable } from 'rxjs';
+import { ChatService } from '../Services/chat.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  users: Observable<User[]>;
+
+  constructor(private chatService: ChatService) { 
+  }
 
   ngOnInit() {
+    this.users = this.chatService.getUsers().valueChanges();    
+  }
+
+  ngOnChanges(){
+    this.ngOnInit();
   }
 
 }
