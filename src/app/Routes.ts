4d05f2fc0d2362be0006fc4bdfcf97773b92d3f3
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-03 12:05:17 -0400

    Add authGuard on chat route to prevent unaythenticated users from navigating

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ChatroomComponent } from './chatroom/chatroom.component';
-import { AuthGuard } from './auth.guard';
+import { AuthGuard } from './Services/auth.guard';
 
 export const appRoutes: Routes = [
     { path: 'signup', component: SignupComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'chat', component: ChatroomComponent },
+    { path: 'chat', canActivate: [AuthGuard], component: ChatroomComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full'},
 ];
