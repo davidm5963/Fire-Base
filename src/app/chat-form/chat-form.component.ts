543f5375be907cdc64e2bc92d9065f60dfff6526
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-03 20:59:30 -0400

    initialize chatForm property OnInit()

--- src/app/chat-form/chat-form.component.ts
+++ src/app/chat-form/chat-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../Services/chat.service'
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-form',
@@ -9,22 +9,25 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ChatFormComponent implements OnInit {
 
+  chatForm: FormGroup;
 
   constructor(private fb: FormBuilder, private chatService: ChatService) { }
 
   ngOnInit() {
-    
+    this.chatForm = this.fb.group({
+      'message' : ['', [Validators.required]]
+    });
   }
 
   send(){
-    this.chatService.sendMessage(this.message);
-    this.message = '';
+    this.chatService.sendMessage(this.message.value);
   }
 
   handleSubmit(event)
   {
     if(event.keycode == 13)
     {
+      console.log('keycode 13 pressed')
       this.send();
     }
   }
