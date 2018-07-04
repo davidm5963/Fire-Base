ab05993915b8c22fd175f412c416939ed5b9fe3c
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-04 11:05:07 -0400

    Add new path for userProfile component

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -3,10 +3,12 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ChatroomComponent } from './chatroom/chatroom.component';
 import { AuthGuard } from './Services/auth.guard';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const appRoutes: Routes = [
     { path: 'signup', component: SignupComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'chat', canActivate: [AuthGuard], component: ChatroomComponent },
+    { path: 'chat', component: ChatroomComponent, canActivate: [AuthGuard] },
+    { path: 'user/:uid', component: UserProfileComponent, canActivate: [AuthGuard] },
     { path: '', redirectTo: '/login', pathMatch: 'full'},
 ];
