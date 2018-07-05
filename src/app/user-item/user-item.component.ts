533b9295630d3f1cdfafefe0a3cff3d18e787bfb
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-05 11:38:01 -0400

    remove unnessacary properties

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -9,16 +9,10 @@ import { User } from '../Models/user.model';
 export class UserItemComponent implements OnInit {
 
   @Input() user: User;
-  displayName: string;
-  email: string;
-  status: string;
 
   constructor() { }
 
   ngOnInit(user = this.user) {
-    this.displayName = user.displayName;
-    this.email = user.email;
-    this.status = user.status;
   }
 
 }
