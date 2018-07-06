9c714ee8e79a7ddaecdc1c1799e3a6f4a0cf2afd
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-06 10:22:16 -0400

    clean up getting isOwnProfile, import storage and firestore

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../Models/user.model';
 import { AuthService } from '../Services/auth.service';
 import { ActivatedRoute } from '@angular/router';
+import { AngularFireStorage, AngularFireUploadTask } from 'angularfire2/storage';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-user-profile',
@@ -14,11 +18,30 @@ export class UserProfileComponent implements OnInit {
   uid: string;
   isOwnProfile: boolean;
 
-  constructor(private authService: AuthService, private route: ActivatedRoute) { }
+  task: AngularFireUploadTask;
+  imageUrl: any;
+
+  // Progress monitoring
+  percentage: Observable<number>;
+
+  snapshot: Observable<any>;
+
+  constructor(private authService: AuthService,
+              private route: ActivatedRoute,
+              private storage: AngularFireStorage,
+              private afs: AngularFirestore) {
+
+                //get user id from route             
+                this.uid = this.route.snapshot.params['uid'];
+                
+                //check if current user is viewing own profile
+                 this.isOwnProfile = this.uid === this.authService.getFirebaseUser().uid;
+
+                 console.log(this.isOwnProfile);
+                 console.log(this.uid)
+               }
 
   ngOnInit() {
-    //get user id from route
-    this.uid = this.route.snapshot.params['uid'];
     
     //get user document reference from firestore using auth service
     this.authService.getUserDocById(this.uid).get().then(doc =>{
@@ -26,13 +49,5 @@ export class UserProfileComponent implements OnInit {
         console.log(this.user);
       }
     );
-    
-    if(this.uid == this.authService.user.uid){
-      this.isOwnProfile = true;
     }
-    else{
-      this.isOwnProfile = false;
-     }
-    }
-
-}
+  } 
