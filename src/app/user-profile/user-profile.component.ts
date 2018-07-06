1d84befa22800b647e76a39b2c3457a156505c11
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-05 21:45:51 -0400

    get user data, check if profile is current users profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../Models/user.model';
+import { AuthService } from '../Services/auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  user: any;
+  uid: string;
+  isOwnProfile: boolean;
+
+  constructor(private authService: AuthService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-  }
+    //get user id from route
+    this.uid = this.route.snapshot.params['uid'];
+    
+    //get user document reference from firestore using auth service
+    this.authService.getUserDocById(this.uid).get().then(doc =>{
+        this.user = doc.data();
+        console.log(this.user);
+      }
+    );
+    
+    if(this.uid == this.authService.user.uid){
+      this.isOwnProfile = true;
+    }
+    else{
+      this.isOwnProfile = false;
+     }
+    }
 
 }
