557b04bdcfc94ef52a7ec92ee1c079b8b6110dc5
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-09 20:44:06 -0400

    update to support user in chatMessage, get profileImage Url

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ChatService } from '../Services/chat.service';
 import { AuthService } from '../Services/auth.service';
 import { ChatMessage } from '../Models/chat-message.model'
+import { AngularFireStorageModule, AngularFireStorage } from 'angularfire2/storage';
 
 @Component({
   selector: 'app-message',
@@ -11,25 +12,27 @@ import { ChatMessage } from '../Models/chat-message.model'
 export class MessageComponent implements OnInit {
 
   @Input() chatMessage: ChatMessage;
-  email: string;
-  displayName: string;
   message: string;
   timeSent: string;
   isOwnMessage: boolean;
+  profileImageUrl: any;
+  profileImagePath: string;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private storage: AngularFireStorage) {
       console.log(authService.getCurrentUser())
       authService.getCurrentUser().subscribe(user =>{
-        this.isOwnMessage = this.email === user.email;
-      })
+        this.isOwnMessage = this.chatMessage.user.email === user.email;
+      });
     
    }
 
   ngOnInit(chatMessage = this.chatMessage) {
     this.message = chatMessage.message;
-    this.email = chatMessage.email;
-    this.displayName = chatMessage.displayName;
     this.timeSent = chatMessage.timeSent;
+
+    this.profileImageUrl = this.storage.ref(chatMessage.user.profileImageUrl).getDownloadURL().subscribe(url => {
+      this.profileImageUrl = url;
+  });
   }
 
 
