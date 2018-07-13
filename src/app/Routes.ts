48dee13ce4827d24ab2ba0301bbd78c8ec69ae2b
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-12 22:53:59 -0400

    route to User Component instead of UserProfile Component

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { ChatroomComponent } from './chatroom/chatroom.component';
 import { AuthGuard } from './Services/auth.guard';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserComponent } from './user/user.component';
 
 export const appRoutes: Routes = [
     { path: 'signup', component: SignupComponent },
     { path: 'login', component: LoginComponent },
     { path: 'chat', component: ChatroomComponent, canActivate: [AuthGuard] },
-    { path: 'user/:uid', component: UserProfileComponent, canActivate: [AuthGuard] },
+    { path: 'user/:uid', component: UserComponent, canActivate: [AuthGuard] },
     { path: '', redirectTo: '/chat', pathMatch: 'full'},
 ];
